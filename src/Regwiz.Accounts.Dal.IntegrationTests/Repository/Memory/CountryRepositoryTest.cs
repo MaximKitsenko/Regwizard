199c353cdbe118a3d362624bb451f42b716887bf
Maxim Kitsenko <maxim.kitsenko.r31@gmail.com>
2019-08-31 02:17:50 +0300

    tests improved

--- src/Regwiz.Accounts.Dal.IntegrationTests/Repository/Memory/CountryRepositoryTest.cs
+++ src/Regwiz.Accounts.Dal.IntegrationTests/Repository/Memory/CountryRepositoryTest.cs
@@ -1,13 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using bonanza.utils.core.IEnumerable;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Regwiz.Accounts.Dal.Dto;
-using Regwiz.Accounts.Dal.IntegrationTests.Utils.Random;
 using Regwiz.Accounts.Dal.Repository;
 using Regwiz.Accounts.Dal.Repository.Memory;
 using Xunit;
@@ -16,7 +15,7 @@ namespace Regwiz.Accounts.Dal.IntegrationTests.Repository.Memory
 {
     public class CountryRepositoryTest : IClassFixture<DbFixture>, IDisposable
     {
-        private ServiceProvider _serviceProvider;
+        private readonly ServiceProvider _serviceProvider;
 
         /// <summary>
         /// The constructor will be called before each test [fact]
@@ -34,7 +33,7 @@ namespace Regwiz.Accounts.Dal.IntegrationTests.Repository.Memory
         public void Dispose()
         {
             var dbContext = _serviceProvider.GetService<RegwizContext>();
-            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureDeleted(); // this will clean the db for each test
         }
 
 
@@ -42,96 +41,71 @@ namespace Regwiz.Accounts.Dal.IntegrationTests.Repository.Memory
         public void ThereIsNoSuchCountry_CreateCountry_CountryCreated()
         {
             //a
-            var Repository = _serviceProvider.GetService<ICountryRepository>();
+            var repository = _serviceProvider.GetService<ICountryRepository>();
             var country = new Country() {Name = "France"};
 
             //a
-            Repository.CreateCountrys(country);
+            repository.CreateCountrys(country);
 
             //a
-            var countries = Repository.ReadAllCountrys().ToListOrAsList();
+            var countries = repository.ReadAllCountrys().ToListOrAsList();
             countries.Should().NotBeNullOrEmpty();
             countries.Should().Contain(x => x.Name == country.Name);
-            Repository.Dispose();
         }
 
         [Fact]
         public void ThereIsSuchCountry_UpdateCountry_CountryCreated()
         {
             //a
-            var Repository = _serviceProvider.GetService<ICountryRepository>();
-            var country = new Country() { Name = "France" };
-            var countriesCreated = Repository.CreateCountrys(country);
-            var countriesCreatedIds = countriesCreated.Select(x => x.Id).ToArray();
+            int[] countriesCreatedIds;
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                var country = new Country() { Name = "France" };
+                var countriesCreated = repository.CreateCountrys(country);
+                countriesCreatedIds = countriesCreated.Select(x => x.Id).ToArray();
+            }
 
             //a
-            var countriesForUpdate = Repository.ReadCountrys(countriesCreatedIds).ToListOrAsList();
-            countriesForUpdate.ForEach(x => x.Name += "100500");
-            Repository.UpdateCountrys(countriesForUpdate.ToArray());
+            List<Country> countriesForUpdate;
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                countriesForUpdate = repository.ReadCountrys(countriesCreatedIds).ToListOrAsList();
+                countriesForUpdate.ForEach(x => x.Name += "100500");
+                repository.UpdateCountrys(countriesForUpdate.ToArray());
+            }
 
             //a
-            var countriesAfterUpdate = Repository.ReadCountrys(countriesCreatedIds).ToListOrAsList();
-            countriesAfterUpdate.Should().NotBeNullOrEmpty();
-            countriesAfterUpdate.Should().BeEquivalentTo(countriesForUpdate);
-            Repository.Dispose();
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                var countriesAfterUpdate = repository.ReadCountrys(countriesCreatedIds).ToListOrAsList();
+                countriesAfterUpdate.Should().NotBeNullOrEmpty();
+                countriesAfterUpdate.Should().BeEquivalentTo(countriesForUpdate, options => options.WithoutStrictOrdering());
+            }
         }
 
-        //[Fact]
-        //public void ThereIsSuchCountry_DeleteCountry_Deleted()
-        //{
-        //    //a
-        //    var Repository = _serviceProvider.GetService<ICountryRepository>();
-        //    var country = new Country() { Name = "France" };
-        //    Repository.CreateCountrys(country);
-
-        //    //a
-        //    Repository.DeleteCountrys(country);
-
-        //    //a
-        //    var countries = Repository.ReadAllCountrys().ToListOrAsList();
-        //    countries.Should().NotContain(x => x.Name == country.Name);
-        //}
-    }
-
-    public class DbFixture
-    {
-        public ServiceProvider ServiceProvider { get; private set; }
-
-        //public TestBase(
-        //    IRandomGenerator randomGenerator
-        //)
-        //{
-        //    this.RandomGenerator = randomGenerator;
-        //}
-
-        public DbFixture()
+        [Fact]
+        public void ThereIsSuchCountry_DeleteCountry_Deleted()
         {
-            //setup our DI
-            var serviceProvider = new ServiceCollection()
-            //.AddLogging()
-                    .AddDbContext<RegwizContext>(options => options.UseInMemoryDatabase("db1"),
-                        ServiceLifetime.Transient)
-                .AddSingleton<IRandomGenerator, SimpleRandomGenerator>()
-                .AddTransient<ICountryRepository, CountryRepository>()
-                .AddSingleton<IRandomGenerator, SimpleRandomGenerator>()
-                //.AddSingleton<IBarService, BarService>()
-                .BuildServiceProvider();
+            //a
+            Country country;
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                country = new Country() {Name = "France"};
+                repository.CreateCountrys(country);
+            }
 
-            //configure console logging
-            //serviceProvider
-            //    .GetService<ILoggerFactory>()
-            //    .AddConsole(LogLevel.Debug);
+            //a
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                repository.DeleteCountrys(country);
+            }
 
-            //var logger = serviceProvider.GetService<ILoggerFactory>()
-            //    .CreateLogger<Program>();
-            //logger.LogDebug("Starting application");
-
-            //do the actual work here
-            //var bar = serviceProvider.GetService<IBarService>();
-            //bar.DoSomeRealWork();
-
-            //logger.LogDebug("All done!");
-            ServiceProvider = serviceProvider;
+            //a
+            using (var repository = _serviceProvider.GetService<ICountryRepository>())
+            {
+                var countries = repository.ReadAllCountrys().ToListOrAsList();
+                countries.Should().NotContain(x => x.Name == country.Name);
+            }
         }
     }
 }
---
+++ src/Regwiz.Accounts.Dal.IntegrationTests/Repository/Memory/DbFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Regwiz.Accounts.Dal.IntegrationTests.Utils.Random;
+using Regwiz.Accounts.Dal.Repository;
+using Regwiz.Accounts.Dal.Repository.Memory;
+
+namespace Regwiz.Accounts.Dal.IntegrationTests.Repository.Memory
+{
+    public class DbFixture
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbFixture()
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                //.AddLogging()
+                .AddDbContext<RegwizContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+                    options.EnableSensitiveDataLogging();
+                },ServiceLifetime.Transient)
+                .AddSingleton<IRandomGenerator, SimpleRandomGenerator>()
+                .AddTransient<ICountryRepository, CountryRepository>()
+                .AddSingleton<IUserRepository, UserRepository>()
+                .BuildServiceProvider();
+
+            //configure console logging
+            //serviceProvider
+            //    .GetService<ILoggerFactory>()
+            //    .AddConsole(LogLevel.Debug);
+
+            //var logger = serviceProvider.GetService<ILoggerFactory>()
+            //    .CreateLogger<Program>();
+            //logger.LogDebug("Starting application");
+
+            //do the actual work here
+            //var bar = serviceProvider.GetService<IBarService>();
+            //bar.DoSomeRealWork();
+
+            //logger.LogDebug("All done!");
+            ServiceProvider = serviceProvider;
+        }
+    }
+}
