1063fac5615381509ec710950c9d1665db8dc4da
Maxim Kitsenko <maxim.kitsenko.r31@gmail.com>
2019-09-03 23:52:34 +0300

    code cleaned

--- src/Regwiz.Accounts.Web/Controllers/SampleDataController.cs
+++ src/Regwiz.Accounts.Web/Controllers/SampleDataController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Regwiz.Accounts.Business.Infrastructure;
+using Regwiz.Accounts.Business.Infrastructure.Command;
 using Regwiz.Accounts.Business.Infrastructure.Query;
 
 namespace Regwiz.Accounts.Web.Controllers
@@ -20,23 +21,6 @@ namespace Regwiz.Accounts.Web.Controllers
             _commandDispatcher = commandDispatcher;
             _queryDispatcher = queryDispatcher;
         }
-
-        //private static string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
-
-        //[HttpGet("[action]")]
-        //public IEnumerable<WeatherForecast> WeatherForecasts()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    });
-        //}
 
         [HttpGet("[action]")]
         public IEnumerable<Country> Countries()
@@ -64,6 +48,21 @@ namespace Regwiz.Accounts.Web.Controllers
             //return Enumerable.Range(1, 5).Select(x=>new Province() {Name = ""+ countryId +"-"+x,Id = x,CountryId = countryId});
         }
 
+
+        [HttpPost("[action]")]
+        public void SaveUser(User user)
+        {
+            Debugger.Break();
+            _commandDispatcher.Execute(new CreateUser(Guid.Empty, user.Login,user.Password,user.Login ){});
+        }
+
+        public class User
+        {
+            public string Login { get; set; }
+            public string Password { get; set; }
+            public int CountryId { get; set; }
+            public string ProvinceId { get; set; }
+        }
         public class Province
         {
             public int Id { get; set; }
@@ -74,21 +73,6 @@ namespace Regwiz.Accounts.Web.Controllers
         {
             public int Id { get; set; }
             public string Name { get; set; }
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
         }
     }
 }
